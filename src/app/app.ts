33b7b03cba5cafc56312469a9774e2eab4f8d787
Samuel Brito <samuelbritospi@gmail.com>
2025-08-04 23:43:32 -0300

    Version 0.0.3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [Home, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
